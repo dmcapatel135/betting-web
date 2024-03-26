d79dc6ce2f8ea4cb9a75656d628816f573ab054b
Dharmendra patel <dharmendra.mongoosetech@gmail.com>
2024-03-26 10:28:27 +0530

    feat : resolve conflict

--- app/components/SportsMenu/index.js
+++ app/components/SportsMenu/index.js
@@ -248,11 +248,12 @@ function SportsMenu() {
               <select
                 value={selectTournament}
                 onChange={(e) => {
+                  console.log('-------------e.target .value ', e.target.value);
                   setSelectTournament(e.target.value);
                 }}
                 className="w-full pl-2 my-2 bg-blue custom-select-drop font-[600] text-12 md:text-14 text-center text-white h-[32px] 2xl:h-[42px]  outline-none  rounded-[4px]"
               >
-                <option>Top Leagues & Countries</option>
+                <option value="">Top Leagues & Countries</option>
                 {allTournaments?.map((item) => {
                   return (
                     <option
